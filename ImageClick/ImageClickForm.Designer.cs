61f34c6208673862935af448d70b961e0648c758
Nicolai Schmid <nicolai@schmid.uno>
2016-06-02 17:11:50 +0200

    Initial commit software working despite Settings and language

---
+++ ImageClick/ImageClickForm.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace ImageClick
+{
+    partial class ImageClickForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.startButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.revealButton = new System.Windows.Forms.Button();
+            this.revealEverythingButton = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(12, 12);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(99, 31);
+            this.startButton.TabIndex = 0;
+            this.startButton.Text = "Spiel starten";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 49);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(841, 447);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // revealButton
+            // 
+            this.revealButton.Location = new System.Drawing.Point(117, 12);
+            this.revealButton.Name = "revealButton";
+            this.revealButton.Size = new System.Drawing.Size(117, 31);
+            this.revealButton.TabIndex = 2;
+            this.revealButton.Text = "Einzeln aufdecken";
+            this.revealButton.UseVisualStyleBackColor = true;
+            this.revealButton.Visible = false;
+            this.revealButton.Click += new System.EventHandler(this.revealButton_Click);
+            // 
+            // revealEverythingButton
+            // 
+            this.revealEverythingButton.Location = new System.Drawing.Point(240, 12);
+            this.revealEverythingButton.Name = "revealEverythingButton";
+            this.revealEverythingButton.Size = new System.Drawing.Size(111, 31);
+            this.revealEverythingButton.TabIndex = 3;
+            this.revealEverythingButton.Text = "Alles aufdecken";
+            this.revealEverythingButton.UseVisualStyleBackColor = true;
+            this.revealEverythingButton.Visible = false;
+            this.revealEverythingButton.Click += new System.EventHandler(this.revealEverythingButton_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 300;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // ImageClickForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(865, 508);
+            this.Controls.Add(this.revealEverythingButton);
+            this.Controls.Add(this.revealButton);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.startButton);
+            this.Name = "ImageClickForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button revealButton;
+        private System.Windows.Forms.Button revealEverythingButton;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ ImageClick/ImageClickForm.cs
@@ -0,0 +1,277 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+
+namespace ImageClick
+{
+    public partial class ImageClickForm : Form
+    {
+
+        string[] picturesStringArray;
+        int difficultyInt = 4;
+        int paddingInt = 5;
+        bool clickBool = false;
+        int imageCountInt = 0;
+        bool runButtonBool = true;
+        bool revealButtonBool = true;
+        int count = 0;
+
+        // used[lines];
+        int[] used;
+        Random random = new Random();
+
+        public static AutoResetEvent arEvent = new AutoResetEvent(false);
+
+        // true means not guessed
+        bool[,] fieldBool;
+        Image image;
+        Graphics graphics;
+
+
+
+        public ImageClickForm()
+        {
+            InitializeComponent();
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+            string pathString = folderBrowserDialog1.SelectedPath;
+            // MessageBox.Show(pathString);
+            if (pathString != "")
+            {
+                if (Directory.Exists(pathString))
+                {
+                    picturesStringArray = Directory.GetFiles(pathString, "*.jpg");
+                    if (picturesStringArray.Length == 0)
+                    {
+                        MessageBox.Show("Keine JPG Dateien im Verzeichnis vorhanden");
+                    }
+                    else
+                    {
+                        image = Image.FromFile(picturesStringArray[0]);
+                        runButtonBool = false;
+                        startButton.Text = "Nächstes Bild";
+                        startButton.Enabled = false;
+                        revealButton.Visible = true;
+                        revealEverythingButton.Visible = true;
+                        gameVoid(picturesStringArray[imageCountInt]);
+                    }
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Maximized;
+            fieldBool = new bool[difficultyInt, difficultyInt];
+            used = new int[difficultyInt];
+
+            //graphics = CreateGraphics();
+
+            resetArray();
+
+           
+        }
+
+        private void gameVoid(string fileString)
+        {
+            image.Dispose();
+            image = Image.FromFile(fileString);
+            drawGrid(image.Width, image.Height, difficultyInt, paddingInt);
+            pictureBox1.Image = image;
+        }
+
+
+        private void drawGrid(int width, int height, int diffi, int padding)
+        {
+            // number of lines (excluding spare)
+            int lineHeightInt = height / diffi;
+            int collumnWidthInt = width / diffi;
+
+            for (int spalten = 0; spalten < diffi; spalten++)
+            {
+                for (int zeilen = 0; zeilen < diffi; zeilen++)
+                {
+                    if (fieldBool[spalten, zeilen])
+                    {
+                        int x1 = zeilen * collumnWidthInt;
+                        int y1 = spalten * lineHeightInt;
+                        int x2 = (zeilen + 1) * collumnWidthInt;
+                        int y2 = (spalten + 1) * lineHeightInt;
+                        drawRect(x1, y1, x2, y2);
+                    }
+                }
+            }
+            
+            
+        }
+
+        private void drawLine(int x1, int y1, int x2, int y2, Pen pen)
+        {
+            
+            using (var graphic = Graphics.FromImage(image))
+            {
+                graphic.DrawLine(pen, x1, y1, x2, y2);
+            }
+
+                //graphics.DrawLine(blackPen, x1, y1, x2, y2);
+
+        }
+
+        private void drawRect(int x1, int y1, int x2, int y2)
+        {
+            int width = (x2 - x1);
+            int height = (y2 - y1);
+
+            Pen blackPen = new Pen(Color.Black, paddingInt);
+            Pen whitePen = new Pen(Color.White, paddingInt);
+            SolidBrush whiteBrush = new SolidBrush(Color.White);
+            Rectangle rect = new Rectangle(x1, y1, width, height);
+            using (var graphic = Graphics.FromImage(image))
+            {
+                graphic.FillRectangle(whiteBrush, rect);
+                graphic.DrawLine(blackPen, x1, y1, x1, y2);
+                graphic.DrawLine(blackPen, x1, y1, x2, y1);
+                graphic.DrawLine(blackPen, x2, y1, x2, y2);
+                graphic.DrawLine(blackPen, x1, y2, x2, y2);
+            }
+        }
+
+        private void revealButton_Click(object sender, EventArgs e)
+        {
+            if (revealButtonBool)
+            {
+                reveal();
+            }
+            else
+            {
+                imageCountInt++;
+                count = 0;
+                revealButtonBool = true;
+                revealButton.Text = "Aufdecken";
+                revealEverythingButton.Enabled = true;
+                resetArray();
+                if (imageCountInt == picturesStringArray.Length)
+                {
+                    MessageBox.Show("Zuende");
+                }
+                else
+                { gameVoid(picturesStringArray[imageCountInt]); }
+
+            }
+        }
+           
+        private void resetArray()
+        {
+            for (int i = 0; i < difficultyInt; i++)
+            {
+                for (int i2 = 0; i2 < difficultyInt; i2++)
+                {
+                    fieldBool[i, i2] = true;
+                }
+            }
+        }
+
+
+
+        private void revealEverythingButton_Click(object sender, EventArgs e)
+        {
+            if (revealButtonBool)
+            {
+                revealEverything();
+            }
+            else
+            {
+                imageCountInt++;
+                count = 0;
+                revealButtonBool = true;
+                revealButton.Text = "Aufdecken";
+                resetArray();
+                if (imageCountInt == picturesStringArray.Length)
+                {
+                    MessageBox.Show("Zuende");
+                }
+                else
+                { gameVoid(picturesStringArray[imageCountInt]); }
+
+            }
+
+        }
+
+        private void revealEverything()
+        {
+            timer1.Enabled = true;
+        }
+        
+
+        private void reveal()
+        {
+            bool buffer = true;
+            int collumnInt = random.Next(0, difficultyInt - 1);
+            int lineInt = random.Next(0, difficultyInt - 1);
+
+            if (fieldBool[collumnInt, lineInt] == false)
+            {
+                if (count != difficultyInt * difficultyInt)
+                {
+                    while (true)
+                    {
+                        collumnInt = random.Next(0, difficultyInt);
+                        lineInt = random.Next(0, difficultyInt);
+                        if (fieldBool[collumnInt, lineInt] == true)
+                        {
+                            break;
+                        }
+                    }
+                    if (count == (difficultyInt * difficultyInt)-1)
+                    {
+                        revealButton.Text = "Nächsten Bild";
+                        revealButtonBool = false;
+                        revealEverythingButton.Enabled = false;
+                    }
+                }
+                else
+                {
+                    
+                }
+
+                fieldBool[collumnInt, lineInt] = false;
+                count++;
+            }
+            else
+            {
+                fieldBool[collumnInt, lineInt] = false;
+                used[lineInt] = collumnInt;
+                count++;
+            }
+            gameVoid(picturesStringArray[imageCountInt]);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (count != difficultyInt * difficultyInt)
+            {
+                reveal();
+                //Thread.Sleep(500);
+                //revealEverything();
+            }
+            else
+            {
+                timer1.Enabled = false;
+                revealButtonBool = false;
+                revealButton.Text = "Nächstes Bild";
+                revealEverythingButton.Enabled = false;
+            }
+        }
+    }
+}
