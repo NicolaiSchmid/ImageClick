d0dfc87fe24925f8d5d8724c2e851a1a3f2dd849
Nicolai Schmid <nicolai@schmid.uno>
2016-06-03 12:45:31 +0200

    Design changesf

--- ImageClick/ImageClickForm.Designer.cs
+++ ImageClick/ImageClickForm.Designer.cs
@@ -32,7 +32,6 @@
             this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
             this.startButton = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
             this.revealButton = new System.Windows.Forms.Button();
             this.revealEverythingButton = new System.Windows.Forms.Button();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
@@ -42,12 +41,16 @@
             // 
             // startButton
             // 
+            this.startButton.BackColor = System.Drawing.Color.White;
+            this.startButton.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.startButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.startButton.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.startButton.Location = new System.Drawing.Point(12, 12);
             this.startButton.Name = "startButton";
-            this.startButton.Size = new System.Drawing.Size(99, 31);
+            this.startButton.Size = new System.Drawing.Size(122, 31);
             this.startButton.TabIndex = 0;
             this.startButton.Text = "Spiel starten";
-            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.UseVisualStyleBackColor = false;
             this.startButton.Click += new System.EventHandler(this.startButton_Click);
             // 
             // pictureBox1
@@ -64,23 +67,31 @@
             // 
             // revealButton
             // 
-            this.revealButton.Location = new System.Drawing.Point(117, 12);
+            this.revealButton.BackColor = System.Drawing.Color.White;
+            this.revealButton.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.revealButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.revealButton.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.revealButton.Location = new System.Drawing.Point(140, 12);
             this.revealButton.Name = "revealButton";
-            this.revealButton.Size = new System.Drawing.Size(117, 31);
+            this.revealButton.Size = new System.Drawing.Size(138, 31);
             this.revealButton.TabIndex = 2;
             this.revealButton.Text = "Einzeln aufdecken";
-            this.revealButton.UseVisualStyleBackColor = true;
+            this.revealButton.UseVisualStyleBackColor = false;
             this.revealButton.Visible = false;
             this.revealButton.Click += new System.EventHandler(this.revealButton_Click);
             // 
             // revealEverythingButton
             // 
-            this.revealEverythingButton.Location = new System.Drawing.Point(240, 12);
+            this.revealEverythingButton.BackColor = System.Drawing.Color.White;
+            this.revealEverythingButton.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.revealEverythingButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.revealEverythingButton.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.revealEverythingButton.Location = new System.Drawing.Point(284, 12);
             this.revealEverythingButton.Name = "revealEverythingButton";
-            this.revealEverythingButton.Size = new System.Drawing.Size(111, 31);
+            this.revealEverythingButton.Size = new System.Drawing.Size(124, 31);
             this.revealEverythingButton.TabIndex = 3;
             this.revealEverythingButton.Text = "Alles aufdecken";
-            this.revealEverythingButton.UseVisualStyleBackColor = true;
+            this.revealEverythingButton.UseVisualStyleBackColor = false;
             this.revealEverythingButton.Visible = false;
             this.revealEverythingButton.Click += new System.EventHandler(this.revealEverythingButton_Click);
             // 
@@ -92,18 +103,23 @@
             // settingsButton
             // 
             this.settingsButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.settingsButton.Location = new System.Drawing.Point(772, 12);
+            this.settingsButton.BackColor = System.Drawing.Color.White;
+            this.settingsButton.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.settingsButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.settingsButton.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.settingsButton.Location = new System.Drawing.Point(738, 12);
             this.settingsButton.Name = "settingsButton";
-            this.settingsButton.Size = new System.Drawing.Size(81, 31);
+            this.settingsButton.Size = new System.Drawing.Size(115, 31);
             this.settingsButton.TabIndex = 4;
             this.settingsButton.Text = "Einstellungen";
-            this.settingsButton.UseVisualStyleBackColor = true;
+            this.settingsButton.UseVisualStyleBackColor = false;
             this.settingsButton.Click += new System.EventHandler(this.settingsButton_Click);
             // 
             // ImageClickForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(865, 508);
             this.Controls.Add(this.settingsButton);
             this.Controls.Add(this.revealEverythingButton);
@@ -123,7 +139,6 @@
         private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
         private System.Windows.Forms.Button startButton;
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.ComponentModel.BackgroundWorker backgroundWorker1;
         private System.Windows.Forms.Button revealButton;
         private System.Windows.Forms.Button revealEverythingButton;
         private System.Windows.Forms.Timer timer1;
