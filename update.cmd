16eb0bedf36750a6ba874631d399ca6ee45cb13d
nicolaiimmanuelschmid <nicolai.immanuel.schmid+github@gmail.com>
2016-06-09 22:04:39 +0200

    Update update.cmd

--- update.cmd
+++ update.cmd
@@ -2,7 +2,7 @@
 echo Updating.....
 timeout /t 3
 
-.\pskill.exe -accepteula ImageClick.exe
+pskill.exe -accepteula ImageClick.exe
 xcopy ImageClick.exe ..\ImageClick.exe /Y
 xcopy language.xml ..\language.xml /Y
 echo Update complete
